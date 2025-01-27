6d729c2e8d9bac1ecd051fbd6091b4d9e1f943d2
jonnishmurda <jjo1@hi.is>
2025-01-27 18:03:20 +0000

    styles

--- src/main.js
+++ src/main.js
@@ -1,27 +1,38 @@
 import './style.css'
 import data from '../data/index.json'
 
-let html = ``
+let html = ``;
 
+/**
+ * validData siktar út ógild gögn
+ * leyfir ekki bókstafinn "ó" og því siktar það út gögnin
+ * aðstoð hjá GPT
+ * 
+ */
 const validData = data.filter(
-  (item => item.title && typeof item.title === 'string' && item.file && typeof item.file === 'string')
+  (item) =>
+    item.title &&
+    typeof item.title === 'string' &&
+    item.file &&
+    typeof item.file === 'string' &&
+    /\.json$/.test(item.file) &&
+    /^[a-zA-Z0-9]+$/.test(item.title)
 );
 
 for (let i = 0; i < validData.length; i++) {
-  html += `<li><a href="/pages/${data[i].file?.replace('.json', '.html')}">${validData[i].title?.toUpperCase()}</a></li>`
+  html += `<li><a href="/pages/${validData[i].file.replace('.json', '.html')}">${validData[i].title.toUpperCase()}</a></li>`;
 }
 
 document.querySelector('#app').innerHTML = `
   <main>
     <header>
       <h1>Verkefni 1</h1>
+      </header>
+      <section class="questions">
       <p>Velkomin/n á spurningalistasíðuna</p>
-    </header>
-    <section class="questions">
     <ul>
       ${html}
     </ul>
     </section>
   </main>
-`
-
+`;
